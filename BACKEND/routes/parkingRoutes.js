2ad76e4ec55a615a6c9538596c04240b47de31c5
Boomika-L <boomika.l2024cse@sece.ac.in>
2026-04-18 07:56:13 +0530

    final commit

--- BACKEND/routes/parkingRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controlller = require("../controllers/parkingController");
-
-router.get("/slots",controlller.getSlot);
-router.post("/book",controlller.bookSlot);
-module.exports = router;
